8fae4a7cb68b120dd06ec9b29b743f15174e3037
Hakanage <148159548+Hakanage@users.noreply.github.com>
2023-10-31 20:12:07 +0100

    Deleted superfluous function

--- main.js
+++ main.js
@@ -1,14 +1,6 @@
 var RoyalMailZones = {"UK":"UK","USA":"World Zone 3", "France":"Europe Zone 1", "Germany":"Europe Zone 1", "Australia":"World Zone 2"};
 var ParcelForceZones = {"UK":"UK","USA":"Zone 10", "France":"Zone 7", "Germany":"Zone 7", "Australia":"Zone 11"};
 
-function getCountryZone(isInsuranceUnder250, country)
-{
-    if(isInsuranceUnder250)
-        return RoyalMailZones[country];
-
-    return ParcelForceZones[country];
-}
-
 function getPostageInfo()
 {
     var DeliveryCompany = document.getElementById("Insurance").value;
