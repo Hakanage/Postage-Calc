d0623fbb0c415ee042bbf8462eb7579e42f70ce9
Hakanage <148159548+Hakanage@users.noreply.github.com>
2023-10-31 20:09:24 +0100

    Deleted extra function

--- main.js
+++ main.js
@@ -1,14 +1,6 @@
 var RoyalMailZones = {"UK":"UK","USA":"World Zone 3", "France":"Europe Zone 1", "Germany":"Europe Zone 1", "Australia":"World Zone 2"};
 var ParcelForceZones = {"UK":"UK","USA":"Zone 10", "France":"Zone 7", "Germany":"Zone 7", "Australia":"Zone 11"};
 
-function getCompany(isInsuranceUnder250)
-{
-    if(isInsuranceUnder250)
-        return "RoyalMail";
-
-    return "ParcelForce";
-}
-
 function getCountryZone(isInsuranceUnder250, country)
 {
     if(isInsuranceUnder250)
@@ -24,7 +16,7 @@ function getPostageInfo()
     
     var Country = document.getElementById("Country").value;
     var CountryZone;
-    if(DeliveryCompany == "RoyalMail")
+    if(DeliveryCompany == "Royal Mail")
     {
         CountryZone = RoyalMailZones[Country];
     }
